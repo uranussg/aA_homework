77d1c8932724fbe5c13c1d2c5b51de4a2a493191
uranussg <uranussg@gmail.com>
2020-01-03 10:01:02 -0800

    add private attreader

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -6,17 +6,21 @@ class Stack
 
     def push(el)
       # adds an element to the stack
-      @store.push(el)
+      store.push(el)
       self
     end
 
     def pop
       # removes one element from the stack
-      @store.pop
+      store.pop
     end
 
     def peek
       # returns, but doesn't remove, the top element in the stack
-      @store[-1]
+      store[-1]
     end
+
+    private
+
+    attr_reader :store
   end
